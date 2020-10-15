fece45799be7ed6c0b1184250c9245186dfc1ba9
VictorBandeira <victormeireles.sonda@br-petrobras.com.br>
2020-10-15 19:07:37 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes} from "@angular/router";
+
+
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo: 'core'},
+  {path: 'core', loadChildren: () => import('./core/core.module').then(module => module.CoreModule)}
+]
+
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/containers/home/home.component';
+import {CoreModule} from "./core/core.module";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { HomeComponent } from './core/containers/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]

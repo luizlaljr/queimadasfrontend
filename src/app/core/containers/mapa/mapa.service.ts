4e5441386dd738aa462656d03ea85297ad01a5f5
luizlaljr <luizlaljr@gmail.com>
2020-11-12 09:12:30 -0300

    Fixado o bug do foginho não aparecer no heroku

--- src/app/core/containers/mapa/mapa.service.ts
+++ src/app/core/containers/mapa/mapa.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class MapaService {
 
-  private _url: string = 'http://queimadas.dgi.inpe.br/queimadas/dados-abertos/download/?utm_campaign=dados-abertos&outputFormat=json&utm_medium=landing-page&time=48h&utm_content=focos_brasil_48h&id=focos_brasil&utm_source=landing-page';
+  private _url: string = 'https://queimadas.dgi.inpe.br/queimadas/dados-abertos/download/?utm_campaign=dados-abertos&outputFormat=json&utm_medium=landing-page&time=48h&utm_content=focos_brasil_48h&id=focos_brasil&utm_source=landing-page';
 
   constructor(private _httpClient: HttpClient) { }
 
